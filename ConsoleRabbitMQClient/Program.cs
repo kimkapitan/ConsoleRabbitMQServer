5ef7d9f482e5cbc4b4e7f29d004a7b43d362d396
Aleksandr Kolmogorov <kimkapitan96@gmail.com>
2020-03-19 11:46:35 +0600

    Add project files.

---
+++ ConsoleRabbitMQClient/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class RpcClient
+{
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly EventingBasicConsumer consumer;
+    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper =
+        new ConcurrentDictionary<string, TaskCompletionSource<string>>();
+
+    public RpcClient()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        replyQueueName = channel.QueueDeclare().QueueName;
+        consumer = new EventingBasicConsumer(channel);
+
+
+
+        consumer.Received += (model, ea) =>
+        {
+            if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out TaskCompletionSource<string> tcs))
+                return;
+            var body = ea.Body;
+            var response = Encoding.UTF8.GetString(body);
+            tcs.TrySetResult(response);
+        };
+    }
+
+    public async Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+    {
+        var props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+
+        var tcs = new TaskCompletionSource<string>();
+        callbackMapper.TryAdd(correlationId, tcs);
+
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(
+            exchange: "Test",
+            routingKey: "1", //клиент отправляет только на route = "1"
+            basicProperties: props,
+            body: messageBytes);
+
+        channel.BasicConsume(
+            consumer: consumer,
+            queue: replyQueueName,
+            autoAck: true);
+
+        cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out var tmp));
+        return await tcs.Task;
+    }
+
+    public void Close()
+    {
+        connection.Close();
+    }
+}
+
+public class Rpc
+{
+    public static async Task Main(string[] args)
+    {
+        var val = args.Length < 1 ? "2" : args[0];
+
+        var rpcClient = new RpcClient();
+
+        Console.WriteLine($" [x] Requesting {val}");
+        var response = await rpcClient.CallAsync(val);
+
+        Console.WriteLine(" [.] Got '{0}'", response);
+        rpcClient.Close();
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleRabbitMQServer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class RPCServer
+{
+    public static void Main(string[] args)
+    {
+        var route = args.Length < 1 ? "1" : args[0]; // устанавливаю route
+
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "rpc_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+        channel.ExchangeDeclare("Test", ExchangeType.Direct);
+        channel.QueueBind("rpc_queue", "Test", route);
+
+        channel.BasicQos(0, 1, false);
+        var consumer = new EventingBasicConsumer(channel);
+        channel.BasicConsume(queue: "rpc_queue",
+          autoAck: false, consumer: consumer);
+        Console.WriteLine(" [x] Awaiting RPC requests");
+
+        consumer.Received += (model, ea) =>
+        {
+            string response = null;
+
+            var body = ea.Body;
+            var props = ea.BasicProperties;
+            var replyProps = channel.CreateBasicProperties();
+            replyProps.CorrelationId = props.CorrelationId;
+
+            try
+            {
+                var message = Encoding.UTF8.GetString(body);
+                int n = int.Parse(message);
+                Console.WriteLine(" [.] req({0})", message);
+                response = (n + 1).ToString(); // увеличиваю значение на еденицу и отправляю обратно
+                Console.WriteLine(" [.] resp({0})", response);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" [.] " + e.Message);
+                response = "";
+            }
+            finally
+            {
+                var responseBytes = Encoding.UTF8.GetBytes(response);
+                channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: responseBytes); //станадартная отправка обратно
+                channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                  multiple: false);
+            }
+        };
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
